202ec0e5c3bac95fd5a3c6ada5004d0ac1d16aba
OLUWATOBI DANIEL <danieloluwatobi765@gmail.com>
2025-01-10 12:24:10 +0100

    add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import Chatbox from "./components/articles/Chatbox/Chatbot";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
     <App />
-    <Chatbox />
   </div>
 );
