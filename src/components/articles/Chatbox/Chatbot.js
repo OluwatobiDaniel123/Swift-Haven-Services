e4ebb2ff59aeb992110b8e19092a4ce27d6d88a6
OLUWATOBI DANIEL <danieloluwatobi765@gmail.com>
2025-01-10 12:20:42 +0100

    add

--- src/components/articles/Chatbox/Chatbot.js
+++ src/components/articles/Chatbox/Chatbot.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Chatbot from "react-chatbot-kit";
-import "react-chatbot-kit/build/main.css";
+// import "react-chatbot-kit/build/main.css";
 import config from "./Config";
 import ActionProvider from "./ActionProvider";
 import MessageParser from "./MessageParser";
